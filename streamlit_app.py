cdec50c4fcea45413458cdc5e7eb634956440014
Dylan Ogden, Ph.D. <67968474+dsogden@users.noreply.github.com>
2025-05-20 17:33:33 +0000

    the chatbot is complete!

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,15 +4,15 @@ from openai import OpenAI
 st.title("ChatGPT-like Clone")
 OPENAI_API_KEY = st.secrets['OPENAI_API_KEY']
 
-def generate_response(input_text: str, client: any) -> None:
-        response = client.responses.create(
-            model=st.session_state['openai_model'],
-            instructions="Talk like a pirate.",
-            input=input_text,
-        )
-        return response.output_text
+# def generate_response(input_text: str, client: any) -> None:
+#         response = client.responses.create(
+#             model=st.session_state['openai_model'],
+#             instructions="Talk like a pirate.",
+#             input=input_text,
+#         )
+#         return response.output_text
 
-if OPENAI_API_KEY.startswith('sk-'):
+if OPENAI_API_KEY is not None:
     client = OpenAI(
         api_key=OPENAI_API_KEY
     )
@@ -21,9 +21,38 @@ if OPENAI_API_KEY.startswith('sk-'):
     if "openai_model" not in st.session_state:
         st.session_state["openai_model"] = "gpt-3.5-turbo"
     
-    input_text = 'What happens when you walk the plank?'
-    response = generate_response(input_text, client)
-    st.write(response)
+    if 'messages' not in st.session_state:
+        st.session_state.messages = []
 
+    for message in st.session_state.messages:
+        with st.chat_message(message['role']):
+            st.markdown(message['content'])
+
+    # st.write('Hello, what I can you help you with?')
+    if prompt := st.chat_input('Ask anything'):
+        st.session_state.messages.append(
+            {
+                'role': 'user',
+                'content': prompt
+            }
+        )
+        with st.chat_message('user'):
+            st.markdown(prompt)
+        with st.chat_message("assistant"):
+            stream = client.chat.completions.create(
+                model=st.session_state["openai_model"],
+                messages=[
+                    {"role": m["role"], "content": m["content"]}
+                    for m in st.session_state.messages
+                ],
+                stream=True,
+            )
+            response = st.write_stream(stream)
+        st.session_state.messages.append(
+            {
+                "role": "assistant",
+                "content": response
+            }
+        )
 else:
     st.write('Please enter your OpenAI key!')
