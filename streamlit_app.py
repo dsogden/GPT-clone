8dbe119632c16235f652893c4647f2e5defb0aba
Dylan Ogden, Ph.D. <67968474+dsogden@users.noreply.github.com>
2025-05-20 16:55:22 +0000

    adding api key settings

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,10 @@
 import streamlit as st
+from openai import OpenAI
+
+OPENAI_API_KEY = st.sidebar.text_input("OpenAI API Key", type="password")
+client = OpenAI(
+    api_key=OPENAI_API_KEY
+)
 
 st.title("🎈 My new app")
 st.write(
