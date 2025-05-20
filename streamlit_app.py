f4446d21efa0feab5669e7f3ca524f20029d5397
Dylan Ogden, Ph.D. <67968474+dsogden@users.noreply.github.com>
2025-05-20 17:20:54 +0000

    basic functionality works and now working on the chat box history

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,29 @@
 import streamlit as st
 from openai import OpenAI
 
-OPENAI_API_KEY = st.sidebar.text_input("OpenAI API Key", type="password")
-st.write('key', OPENAI_API_KEY)
-if OPENAI_API_KEY is not None:
-    st.write('accepted the key')
-    # client = OpenAI(
-    #     api_key=OPENAI_API_KEY
-    # )
+st.title("ChatGPT-like Clone")
+OPENAI_API_KEY = st.secrets['OPENAI_API_KEY']
 
-    # # Set a default model
-    # if "openai_model" not in st.session_state:
-    #     st.session_state["openai_model"] = "gpt-3.5-turbo"
+def generate_response(input_text: str, client: any) -> None:
+        response = client.responses.create(
+            model=st.session_state['openai_model'],
+            instructions="Talk like a pirate.",
+            input=input_text,
+        )
+        return response.output_text
 
+if OPENAI_API_KEY.startswith('sk-'):
+    client = OpenAI(
+        api_key=OPENAI_API_KEY
+    )
 
-    #     response = client.responses.create(
-    #         model=st.session_state['openai_model'],
-    #         instructions="Talk like a pirate.",
-    #         input="Are semicolons optional in JavaScript?",
-    #     )
+    # Set a default model
+    if "openai_model" not in st.session_state:
+        st.session_state["openai_model"] = "gpt-3.5-turbo"
+    
+    input_text = 'What happens when you walk the plank?'
+    response = generate_response(input_text, client)
+    st.write(response)
 
-    #     st.write(response.output_text)
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+else:
+    st.write('Please enter your OpenAI key!')
