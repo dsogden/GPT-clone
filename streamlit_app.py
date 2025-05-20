d63a63c4e287b84916b5dd9f71c071b59b409ea7
Dylan Ogden, Ph.D. <67968474+dsogden@users.noreply.github.com>
2025-05-20 17:01:04 +0000

    testing client responses on a dummy input

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,22 +3,23 @@ from openai import OpenAI
 
 OPENAI_API_KEY = st.sidebar.text_input("OpenAI API Key", type="password")
 
-client = OpenAI(
-    api_key=OPENAI_API_KEY
-)
-
-# Set a default model
-if "openai_model" not in st.session_state:
-    st.session_state["openai_model"] = "gpt-3.5-turbo"
-
 if OPENAI_API_KEY is not None:
-    response = client.responses.create(
-        model=st.session_state['openai_model'],
-        instructions="Talk like a pirate.",
-        input="Are semicolons optional in JavaScript?",
+    client = OpenAI(
+        api_key=OPENAI_API_KEY
     )
 
-    print(response.output_text)
+    # Set a default model
+    if "openai_model" not in st.session_state:
+        st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+
+        response = client.responses.create(
+            model=st.session_state['openai_model'],
+            instructions="Talk like a pirate.",
+            input="Are semicolons optional in JavaScript?",
+        )
+
+        st.write(response.output_text)
 
 st.title("🎈 My new app")
 st.write(
