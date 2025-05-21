e76488b890af2ac65d8efb3557f9caf214e71263
Dylan Ogden, Ph.D. <67968474+dsogden@users.noreply.github.com>
2025-05-21 15:17:34 +0000

    updating and adding the new langchain functionality

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,14 @@
 import streamlit as st
 from langchain_openai import ChatOpenAI
-from langchain_core.messages import HumanMessage, SystemMessage
-# from openai import OpenAI
+from langchain_core.messages import HumanMessage
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import START, MessagesState, StateGraph
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
-
 st.title("ChatGPT-like Clone")
+
+load_dotenv()
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 os.environ["LANGSMITH_API_KEY"] = os.getenv("LANGSMITH_API_KEY")
@@ -16,16 +17,45 @@ os.environ["LANGSMITH_ENDPOINT"] = os.getenv('LANGSMITH_ENDPOINT')
 
 model = ChatOpenAI(model='gpt-3.5-turbo')
 
-# messages = [
-#     SystemMessage('Translate the following form English to German'),
-#     HumanMessage('What is the date?')
-# ]
+# Define the function that calls the model
+def call_model(state: MessagesState):
+    response = model.invoke(state["messages"])
+    return {"messages": response}
 
-prompt = st.chat_input("Ask anything")
-if prompt:
-    with st.chat_message('user'):
-        st.markdown(prompt)
+# Define a new graph
+workflow = StateGraph(state_schema=MessagesState)
 
-# st.write(
-#     model.invoke(messages).content
-# )
+# Define the (single) node in the graph
+workflow.add_edge(START, "model")
+workflow.add_node("model", call_model)
+
+# Add memory
+memory = MemorySaver()
+app = workflow.compile(checkpointer=memory)
+config = {"configurable": {"thread_id": "abc123"}}
+
+if 'messages' not in st.session_state:
+    st.session_state.messages = []
+
+def main():
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    if prompt := st.chat_input("Ask anything"):
+        st.session_state.messages.append(
+            {"role": "user", "content": prompt}
+        )
+        with st.chat_message('user'):
+            st.markdown(prompt)
+            input_message = [HumanMessage(prompt)]
+            response = app.invoke({"messages": input_message}, config)
+        with st.chat_message('assistant'):
+            st.markdown(response['messages'][-1].content)
+
+        st.session_state.messages.append(
+            {"role": "assistant", "content": response['messages'][-1].content}
+        )
+
+if __name__ == '__main__':
+    main()
